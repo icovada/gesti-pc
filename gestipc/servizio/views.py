1ace5bf1df96f8d222a575958473f0ab894a34ab
icovada <federico@tabbo.it>
2023-07-30 18:20:22 +0200

    Base Servizio homepage

--- gestipc/servizio/views.py
+++ gestipc/servizio/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
+from django.db.models import Count, OuterRef, Q, Subquery, Func, F
 
-from .models import Servizio
+
+from .models import Servizio, ServizioResponse
 
 # Create your views here.
 
 def main(request):
-    servizi = Servizio.objects.order_by('begin_date').all()
+    total_response = ServizioResponse.objects.filter(id=OuterRef('id')) \
+        .annotate(count=Func(F('id'), function='Count')) \
+        .values('count')
+    total_acks = ServizioResponse.objects.filter(id=OuterRef('id'), response=1) \
+        .annotate(count=Func(F('id'), function='Count')) \
+        .values('count')
+    servizi = Servizio.objects.order_by('-begin_date') \
+        .annotate(total_response=Subquery(total_response),
+                  total_acks=Subquery(total_acks)
+    )
     return render(request, 'servizio/main.html', {"servizi": servizi})
