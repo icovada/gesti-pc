48d5b576a5d75b9cd7e5d21c732eefe35905ec53
icovada <federico@tabbo.it>
2023-07-30 16:01:33 +0200

    Image is of image type

--- gestipc/core/models.py
+++ gestipc/core/models.py
@@ -7,7 +7,7 @@ from django.dispatch import receiver
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    profile_picture = models.FileField()
+    profile_picture = models.ImageField()
 
 
 @receiver(post_save, sender=User)
