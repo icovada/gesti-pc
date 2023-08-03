67d250d7d7305b6b8671557e0a458a149f99ef70
icovada <federico@tabbo.it>
2023-08-03 19:54:51 +0200

    Generate profile automatically

--- gestipc/hr/views.py
+++ gestipc/hr/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 
+from django.core.serializers import deserialize
 from django.contrib.auth.models import User
 from .models import TelegramLink
-from core.models import Profile
 from django.contrib.auth.decorators import login_required
 from pcroncellobot.bot import bot
 from pcroncellobot.processors import registration_complete
 
 # Create your views here.
+
 
 @login_required
 def main(request):
@@ -19,9 +20,21 @@ def main(request):
 @login_required
 def detail_page(request, id):
     user = User.objects.get(id=id)
+    userprofile = user.profile
+
+    profile_fields = userprofile._meta.get_fields()
+
+    exclude = ['fkuser', 'address', 'telegram_user', 'profile_picture']
+
+    profile_dict = {
+        x.verbose_name: getattr(userprofile, x.name)
+        for x in profile_fields if x.name not in exclude
+    }
 
     return render(request, 'hr/profile.html', {"curpage": "volontari",
-                                               "user": user})
+                                               "user": user,
+                                               "profile": profile_dict})
+
 
 @login_required
 def link_tg(request, uuid):
