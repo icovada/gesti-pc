50bf95fa2e7cf1f61f8c0ac63744bed0b0ed7ee7
icovada <federico@tabbo.it>
2026-01-21 14:21:23 +0100

    register LoginToken to admin page

--- src/tg_bot/admin.py
+++ src/tg_bot/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import TelegramUser
+from .models import LoginToken, TelegramUser
 
 @admin.register(TelegramUser)
 class TelegramUserAdmin(admin.ModelAdmin):
@@ -35,3 +35,28 @@ class TelegramUserAdmin(admin.ModelAdmin):
     def has_add_permission(self, request):
         return self.has_module_permission(request)
 
+
+@admin.register(LoginToken)
+class LoginTokenAdmin(admin.ModelAdmin):
+    list_display = ["token", "telegram_user", "created_at", "used_at", "is_valid"]
+    list_filter = ["created_at", "used_at"]
+    search_fields = ["token", "telegram_user__username", "telegram_user__telegram_id"]
+    raw_id_fields = ["telegram_user"]
+    readonly_fields = ["token", "telegram_user", "created_at", "used_at"]
+
+    def has_module_permission(self, request):
+        if request.user.is_superuser:
+            return True
+        return request.user.groups.filter(name="IT Admin").exists()
+
+    def has_view_permission(self, request, obj=None):
+        return self.has_module_permission(request)
+
+    def has_change_permission(self, request, obj=None):
+        return self.has_module_permission(request)
+
+    def has_delete_permission(self, request, obj=None):
+        return self.has_module_permission(request)
+
+    def has_add_permission(self, request):
+        return False
